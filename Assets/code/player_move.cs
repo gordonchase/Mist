679f0d2f828c257b44cf0d3d200388e835ef8d8e
Something <gordon.chase@meritknights.com>
2025-11-18 15:13:41 -0700

    glar

--- Assets/code/player_move.cs
+++ Assets/code/player_move.cs
@@ -32,11 +32,13 @@ public class PlayerController : MonoBehaviour
     public int tinbarpercent=500;
     public int pewterbarpercent=100;
     public int steelbarpercent=200;
-    public int ironbarpercent=200
+    public int ironbarpercent=200;
 
-
-
-
+    // ===== ORIGINAL FUNCTION =====
+    void linechooser()
+    {
+        Time.timeScale = 0.2f;
+    }
 
     void Start()
     {
@@ -48,228 +50,175 @@ public class PlayerController : MonoBehaviour
         xSpeed = 3.5f;
         jumpStrength = 300;
         notrealA = 1.0f;
-
     }
 
     IEnumerator DoEverySecond()
     {
         while (true)
         {
-
             if (buringtin && tinbarpercent>0)
             {
-            if (flaring)
-            {
-            tinbarpercent -= 2;
-            }
-            else
-            {
-            tinbarpercent -= 1;
-            }
+                if (flaring) tinbarpercent -= 2;
+                else tinbarpercent -= 1;
             }
             if (buringpewter && pewterbarpercent>0)
             {
-            if (flaring)
-            {
-            pewterbarpercent -= 2;
-            }
-            else
-            {
-            pewterbarpercent -= 1;
-            }
+                if (flaring) pewterbarpercent -= 2;
+                else pewterbarpercent -= 1;
             }
             if (buringsteel && steelbarpercent>0)
             {
-            if (flaring)
-            {
-            steelbarpercent -= 2;
-            }
-            else
-            {
-            steelbarpercent -= 1;
-            }
+                if (flaring) steelbarpercent -= 2;
+                else steelbarpercent -= 1;
             }
             if (buringiron && ironbarpercent>0)
             {
-            if (flaring)
-            {
-            ironbarpercent -= 2;
+                if (flaring) ironbarpercent -= 2;
+                else ironbarpercent -= 1;
             }
-            else
-            {
-            ironbarpercent -= 1;
-            }
-            }
-            yield return new WaitForSeconds(1f);  // wait 1 second
+            yield return new WaitForSeconds(1f);
         }
     }
 
     void Update()
-{
-    if (Input.GetKeyDown(KeyCode.Z))
     {
-        if (buringtin){
-            mistpsA = 0.075f;
-            buringtin = false;
-            notrealA = 1.0f;
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (buringtin)
+            {
+                mistpsA = 0.075f;
+                buringtin = false;
+                notrealA = 1.0f;
             }
-        else{
-            mistpsA = 0.015f;
-            buringtin = true;
-            notrealA = 1.0f;
+            else
+            {
+                mistpsA = 0.015f;
+                buringtin = true;
+                notrealA = 1.0f;
             }
-        
-    }
+        }
 
-    if (Input.GetKeyDown(KeyCode.X))
-    {
-        if (buringpewter){
-            xSpeed = 3.5f;
-            jumpStrength = 300;
-            buringpewter = false;
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (buringpewter)
+            {
+                xSpeed = 3.5f;
+                jumpStrength = 300;
+                buringpewter = false;
             }
-        else{
-            xSpeed = 7;
-            jumpStrength = 450;
-            buringpewter = true;
+            else
+            {
+                xSpeed = 7;
+                jumpStrength = 450;
+                buringpewter = true;
             }
-        
-    }
+        }
 
-    if (Input.GetKeyDown(KeyCode.C))
-    {
-        if (buringsteel){
-            buringsteel = false;
-            }
-        else{
-            buringsteel = true;
-            }
-        
-    }
-    if (Input.GetKeyDown(KeyCode.V))
-    {
-        if (buringiron){
-            buringiron = false;
-            }
-        else{
-            buringiron = true;
-            }
-        
-    }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (buringsteel) buringsteel = false;
+            else buringsteel = true;
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (buringiron) buringiron = false;
+            else buringiron = true;
+        }
+
         if (Input.GetKeyDown(KeyCode.F))
-    {
-        if (flaring){
-            flaring = false;
-            }
-        else{
-            flaring = true;
-            }
-        
+        {
+            if (flaring) flaring = false;
+            else flaring = true;
+        }
+
+        // ===== NEW: Activate line chooser for steel/iron =====
+        if (Input.GetKeyDown(KeyCode.C) && buringsteel)
+        {
+            ActivateLineChooser();
+        }
+        if (Input.GetKeyDown(KeyCode.V) && buringiron)
+        {
+            ActivateLineChooser();
+        }
     }
-
-}
-
 
     void FixedUpdate()
     {
-
-
-    
-    if (flaring){
-        if (buringtin){
-            mistpsA = 0.002f;
-            notrealA = 0.3f;
+        if (flaring)
+        {
+            if (buringtin)
+            {
+                mistpsA = 0.002f;
+                notrealA = 0.3f;
             }
-        if (buringpewter){
-            xSpeed = 10;
-            jumpStrength = 650;
-            }  
-        if (buringsteel){
-            }               
-        if (buringiron){
-            }               
+            if (buringpewter)
+            {
+                xSpeed = 10;
+                jumpStrength = 650;
+            }
         }
-        else{
-        if (buringtin){
-            mistpsA = 0.015f;
-            notrealA = 1.0f;
+        else
+        {
+            if (buringtin)
+            {
+                mistpsA = 0.015f;
+                notrealA = 1.0f;
             }
-        if (buringpewter){
-            xSpeed = 7;
-            jumpStrength = 450;
+            if (buringpewter)
+            {
+                xSpeed = 7;
+                jumpStrength = 450;
             }
-        if (buringsteel){
-            }    
-        if (buringiron){
-            }          
         }
 
-    if (tinbarpercent < 1)
+        if (tinbarpercent < 1)
         {
-        buringtin = false;  
-        mistpsA = 0.075f; 
-        }   
-    if (pewterbarpercent < 1)
+            buringtin = false;
+            mistpsA = 0.075f;
+        }
+        if (pewterbarpercent < 1)
         {
-        buringpewter = false;  
-        xSpeed = 3.5f;
-        jumpStrength = 300;
-        }  
-    if (steelbarpercent < 1)
-        {
-        buringsteel = false;  
-        }    
-    if (ironbarpercent < 1)
-        {
-        buringiron = false;  
-        }        
+            buringpewter = false;
+            xSpeed = 3.5f;
+            jumpStrength = 300;
+        }
 
-BoxCollider2D box = GetComponent<BoxCollider2D>();
-Vector2 rayOrigin = (Vector2)transform.position + box.offset;
+        BoxCollider2D box = GetComponent<BoxCollider2D>();
+        Vector2 rayOrigin = (Vector2)transform.position + box.offset;
+        float halfWidth = box.size.x * 0.5f * transform.localScale.x;
+        float rayDistance = groundCheckDistance;
+        int groundLayer = LayerMask.GetMask("ground");
 
-float halfWidth = box.size.x * 0.5f * transform.localScale.x;
-float rayDistance = groundCheckDistance;
-int groundLayer = LayerMask.GetMask("ground");
+        Vector2 leftEdge = rayOrigin + Vector2.left * halfWidth;
+        Vector2 center = rayOrigin;
+        Vector2 rightEdge = rayOrigin + Vector2.right * halfWidth;
 
+        RaycastHit2D leftRay = Physics2D.Raycast(leftEdge, Vector2.down, rayDistance, groundLayer);
+        RaycastHit2D centerRay = Physics2D.Raycast(center, Vector2.down, rayDistance, groundLayer);
+        RaycastHit2D rightRay = Physics2D.Raycast(rightEdge, Vector2.down, rayDistance, groundLayer);
 
-Vector2 leftEdge = rayOrigin + Vector2.left * halfWidth;
-Vector2 center = rayOrigin;
-Vector2 rightEdge = rayOrigin + Vector2.right * halfWidth;
+        Debug.DrawRay(leftEdge, Vector2.down * rayDistance, Color.red);
+        Debug.DrawRay(center, Vector2.down * rayDistance, Color.green);
+        Debug.DrawRay(rightEdge, Vector2.down * rayDistance, Color.blue);
 
-RaycastHit2D leftRay = Physics2D.Raycast(leftEdge, Vector2.down, rayDistance, groundLayer);
-RaycastHit2D centerRay = Physics2D.Raycast(center, Vector2.down, rayDistance, groundLayer);
-RaycastHit2D rightRay = Physics2D.Raycast(rightEdge, Vector2.down, rayDistance, groundLayer);
-
-
-Debug.DrawRay(leftEdge, Vector2.down * rayDistance, Color.red);
-Debug.DrawRay(center, Vector2.down * rayDistance, Color.green);
-Debug.DrawRay(rightEdge, Vector2.down * rayDistance, Color.blue);
-
-
-isGrounded = leftRay.collider != null || centerRay.collider != null || rightRay.collider != null;
-
+        isGrounded = leftRay.collider != null || centerRay.collider != null || rightRay.collider != null;
 
         float xHat = Input.GetAxisRaw("Horizontal");
         float vx = xHat * xSpeed;
         rb.linearVelocity = new Vector2(vx, rb.linearVelocity.y);
 
-        //float horizontal = xHat;
-        //if (Mathf.Abs(horizontal) < 0.01f) horizontal = 0f;
-        //anim.SetFloat("Horizontal", horizontal);
-
         float yHat = new Vector2(0, Input.GetAxis("Vertical")).normalized.y;
-        if (isGrounded && yHat == 1) {
+        if (isGrounded && yHat == 1)
+        {
             float vy = yHat * jumpStrength;
             isGrounded = false;
             rb.AddForce(transform.up * vy);
-
-
         }
 
         anim.SetFloat("ySpeed", rb.linearVelocity.y);
         anim.SetFloat("xSpeed", rb.linearVelocity.x);
 
-        var main = mistps.main; 
+        var main = mistps.main;
         Color c = main.startColor.color;
         c.a = mistpsA;
         main.startColor = new ParticleSystem.MinMaxGradient(c);
@@ -278,6 +227,16 @@ isGrounded = leftRay.collider != null || centerRay.collider != null || rightRay.
         tileColor.a = notrealA;
         notreal.color = tileColor;
 
+        // ===== NEW: continuous pull/push if a target is selected =====
+        if (selectedTarget != null)
+        {
+            Rigidbody2D rbMetal = selectedTarget.GetComponent<Rigidbody2D>();
+            if (rbMetal != null)
+            {
+                Vector2 dir = (selectedTarget.position - transform.position).normalized;
+                rbMetal.AddForce(-dir * pullStrength * Time.fixedDeltaTime);
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -286,61 +245,171 @@ isGrounded = leftRay.collider != null || centerRay.collider != null || rightRay.
         {
             Destroy(collision.gameObject);
             tinbarpercent += 250;
-            if (tinbarpercent > 500)
-            {
-            tinbarpercent = 500;    
-            }
+            if (tinbarpercent > 500) tinbarpercent = 500;
         }
         if (collision.gameObject.CompareTag("Pewter"))
         {
             Destroy(collision.gameObject);
             pewterbarpercent += 50;
-            if (pewterbarpercent > 100)
-            {
-            pewterbarpercent = 100;    
-            }
+            if (pewterbarpercent > 100) pewterbarpercent = 100;
         }
         if (collision.gameObject.CompareTag("Steel"))
         {
             Destroy(collision.gameObject);
             steelbarpercent += 100;
-            if (steelbarpercent > 200)
-            {
-            steelbarpercent = 200;    
-            }
-        }       
+            if (steelbarpercent > 200) steelbarpercent = 200;
+        }
         if (collision.gameObject.CompareTag("Iron"))
         {
             Destroy(collision.gameObject);
             ironbarpercent += 100;
-            if (ironbarpercent > 200)
-            {
-            ironbarpercent = 200;       
-            }
-        }  
+            if (ironbarpercent > 200) ironbarpercent = 200;
+        }
         if (collision.gameObject.CompareTag("Aitum"))
         {
             Destroy(collision.gameObject);
             pewterbarpercent += 25;
-            if (pewterbarpercent > 100)
-            {
-            pewterbarpercent = 100;    
-            }
+            if (pewterbarpercent > 100) pewterbarpercent = 100;
             tinbarpercent += 125;
-            if (tinbarpercent > 500)
-            {
-            tinbarpercent = 500;    
-            }
+            if (tinbarpercent > 500) tinbarpercent = 500;
             steelbarpercent += 50;
-            if (steelbarpercent > 200)
-            {
-            steelbarpercent = 200;    
-            }
+            if (steelbarpercent > 200) steelbarpercent = 200;
             ironbarpercent += 50;
-            if (ironbarpercent > 200)
+            if (ironbarpercent > 200) ironbarpercent = 200;
+        }
+    }
+
+    // ======= NEW VARIABLES =======
+    public GameObject linePrefab;           // prefab with LineRenderer
+    public float metalDetectRange = 8f;     
+    public LayerMask metalLayer;            
+    private List<Transform> metalTargets = new List<Transform>();
+    private List<LineRenderer> lines = new List<LineRenderer>();
+    private int selectedIndex = 0;
+    private bool isLineChooserActive = false;
+    private Transform selectedTarget = null;
+    private float pullStrength = 500f; // force strength
+
+    void ActivateLineChooser()
+    {
+        isLineChooserActive = true;
+        Time.timeScale = 0.2f;
+        selectedTarget = null;
+        FindMetalTargets();
+        CreateLines();
+        HighlightSelectedLine();
+    }
+
+    void FindMetalTargets()
+    {
+        metalTargets.Clear();
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, metalDetectRange, metalLayer);
+        foreach (Collider2D col in hits)
+        {
+            if (col.isTrigger) continue;
+            metalTargets.Add(col.transform);
+        }
+    }
+
+    void CreateLines()
+    {
+        DestroyAllLines();
+        foreach (Transform metal in metalTargets)
+        {
+            GameObject lineObj = Instantiate(linePrefab);
+            LineRenderer lr = lineObj.GetComponent<LineRenderer>();
+            lr.positionCount = 2;
+            lr.startColor = Color.cyan;
+            lr.endColor = Color.cyan;
+            lr.startWidth = 0.05f;
+            lr.endWidth = 0.05f;
+            lines.Add(lr);
+        }
+    }
+
+    void UpdateLines()
+    {
+        if (!isLineChooserActive) return;
+        for (int i = 0; i < lines.Count; i++)
+        {
+            if (lines[i] == null) continue;
+            lines[i].SetPosition(0, transform.position);
+            lines[i].SetPosition(1, metalTargets[i].position);
+        }
+    }
+
+    void HighlightSelectedLine()
+    {
+        for (int i = 0; i < lines.Count; i++)
+        {
+            if (lines[i] == null) continue;
+            if (i == selectedIndex)
             {
-            ironbarpercent = 200;    
+                lines[i].startColor = Color.blue;
+                lines[i].endColor = Color.blue;
+                lines[i].startWidth = 0.1f;
+                lines[i].endWidth = 0.1f;
+            }
+            else
+            {
+                lines[i].startColor = Color.cyan;
+                lines[i].endColor = Color.cyan;
+                lines[i].startWidth = 0.05f;
+                lines[i].endWidth = 0.05f;
             }
         }
+    }
+
+    void DestroyAllLines()
+    {
+        foreach (LineRenderer lr in lines)
+        {
+            if (lr != null)
+                Destroy(lr.gameObject);
+        }
+        lines.Clear();
+    }
+
+    void HandleLineChooserInput()
+    {
+        if (!isLineChooserActive) return;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            selectedIndex--;
+            if (selectedIndex < 0) selectedIndex = metalTargets.Count - 1;
+            HighlightSelectedLine();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            selectedIndex++;
+            if (selectedIndex >= metalTargets.Count) selectedIndex = 0;
+            HighlightSelectedLine();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            isLineChooserActive = false;
+            Time.timeScale = 1f;
+            DestroyAllLines();
+
+            selectedTarget = metalTargets[selectedIndex];
+            Debug.Log("Selected metal: " + selectedTarget.name);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isLineChooserActive = false;
+            Time.timeScale = 1f;
+            DestroyAllLines();
+            selectedTarget = null;
+        }
+    }
+
+    void LateUpdate()
+    {
+        UpdateLines();
+        HandleLineChooserInput();
     }
 }
