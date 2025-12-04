87aaba02e6461a30dd293fc2195427d3993ca088
Something <gordon.chase@meritknights.com>
2025-12-04 15:15:30 -0700

    work on enemy

--- Assets/code/sm_mistwrath.cs
+++ Assets/code/sm_mistwrath.cs
@@ -3,36 +3,62 @@ using UnityEngine;
 public class sm_mistwrath : MonoBehaviour
 {
     public GameObject playerobject;
-    public GameObject meobject;
+    public GameObject enemyobject;
     public Rigidbody2D rb;
-    public float xSpeed = 10f;
-
-    // cached collider refs
-    private Collider2D playerCollider;
-    private Collider2D meCollider;
-
-    // cached positions (world-space)
-    private float playerx;
-    private float playery;
-    private float mex;
-    private float mey;
-
+    public float checkdistance = 0;
+    public int seedistance = 0;
+    public int speed = 10;
+    public int jump = 20;
+    private Vector2 playerPos;
+    private Vector2 enemyPos;
+    private bool attaking = false;
+    public bool isgrounded = true;
+    private float currentspeed;
+    public bool canmove = true;
+    public float speedcap = 5;
+    
     void Start()
     {
-    playerCollider = playerobject.GetComponent<Collider2D>();
-    meCollider = meobject.GetComponent<Collider2D>();
-    Vector2 playerPos = playerCollider.bounds.center;
-    Vector2 mePos = meCollider.bounds.center;
-    float playerx = playerPos.x;
-    float playery = playerPos.y;
-    float mex = mePos.x;
-    float mey = mePos.y;
+    Vector2 playerPos = playerobject.transform.position;
+    Vector2 enemyPos = enemyobject.transform.position;
+    rb = enemyobject.GetComponent<Rigidbody2D>();
+    checkdistance = Vector2.Distance(playerPos, enemyPos);
     }
     void Update()
     {
-    if ((float)(mex-playerx)>0)
-    rb.AddForce(Vector2.left * xSpeed);
-    if ((float)(mex-playerx)<0)
-    rb.AddForce(Vector2.right * xSpeed);
+    Vector2 playerPos = playerobject.transform.position;
+    Vector2 enemyPos = enemyobject.transform.position;
+    checkdistance = Vector2.Distance(playerPos, enemyPos);
+    if (checkdistance<seedistance){
+        if (playerPos.x-enemyPos.x>0 && canmove)
+        {
+        rb.AddForce(Vector2.right * speed, ForceMode2D.Force);
+        }
+        if (playerPos.x-enemyPos.x<0 && canmove)
+        {
+        rb.AddForce(Vector2.left * speed, ForceMode2D.Force);
+        }
+        if (playerPos.y-enemyPos.y>1 && isgrounded)
+        {
+        rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        isgrounded = false;
+        }
+    }
+    float currentspeed = rb.linearVelocity.x;
+    if (Mathf.Abs(currentspeed)>speedcap){
+        canmove = false;
+    }
+        else{
+        canmove = true;
+    }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+    if (collision.gameObject.CompareTag("ground"))  
+        {  
+        isgrounded = true;
+        }  
     }
 }
